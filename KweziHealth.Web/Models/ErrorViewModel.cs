592453928814ac0f30ab807d54c7a7655caf8cad
Tristan Wilkinson <tristanwilkinson0803@gmail.com>
2026-08-12 14:15:32 +0200

    chore: initial commit and MVC project scaffold

---
+++ KweziHealth.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KweziHealth.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
