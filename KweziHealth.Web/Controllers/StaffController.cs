f79c0946f97c264c91016b67c9468263477a7b89
Tristan Wilkinson <tristanwilkinson0803@gmail.com>
2026-08-16 12:56:32 +0200

    chore: Refactored the IService to be async.

--- KweziHealth.Web/Controllers/StaffController.cs
+++ KweziHealth.Web/Controllers/StaffController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using KweziHealth.Web.Models;
 using KweziHealth.Web.Services;
 
@@ -15,27 +16,79 @@ namespace KweziHealth.Web.Controllers
             _staffService = staffService;
         }
 
-        public IActionResult Index()
+        // GET: Staff
+        public async Task<IActionResult> Index()
         {
-            var staffList = _staffService.GetAllStaff();
+            var staffList = await _staffService.GetAllStaffAsync();
             return View(staffList);
         }
 
+        // GET: Staff/Create
         public IActionResult Create()
         {
             return View();
         }
 
+        // POST: Staff/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(StaffMember staff)
+        public async Task<IActionResult> Create(StaffMember staff)
         {
             if (ModelState.IsValid)
             {
-                _staffService.AddStaff(staff);
+                await _staffService.AddStaffAsync(staff);
                 return RedirectToAction(nameof(Index));
             }
             return View(staff);
+        }
+
+        // GET: Staff/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var staff = await _staffService.GetStaffByIdAsync(id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: Staff/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, StaffMember staff)
+        {
+            if (id != staff.StaffId)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _staffService.UpdateStaffAsync(staff);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(staff);
+        }
+
+        // GET: Staff/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var staff = await _staffService.GetStaffByIdAsync(id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: Staff/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _staffService.DeleteStaffAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- KweziHealth.Web/Services/StaffService.cs
+++ KweziHealth.Web/Services/StaffService.cs
@@ -2,48 +2,49 @@ using System.Collections.Generic;
 using System.Linq;
 using KweziHealth.Web.Models;
 using KweziHealth.Web.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace KweziHealth.Web.Services
 {
-    public class StaffService : IStaffService
+   public class StaffService : IStaffService
     {
         private readonly ApplicationDbContext _context;
 
-        // Inject the ApplicationDbContext via the constructor
         public StaffService(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public IEnumerable<StaffMember> GetAllStaff()
+        public async Task<IEnumerable<StaffMember>> GetAllStaffAsync()
         {
-            return _context.StaffMembers.ToList();
+            return await _context.StaffMembers.ToListAsync();
         }
 
-        public StaffMember GetStaffById(int id)
+        public async Task<StaffMember> GetStaffByIdAsync(int id)
         {
-            return _context.StaffMembers.Find(id); // .Find is highly optimized for primary keys
+            return await _context.StaffMembers.FindAsync(id) 
+                ?? throw new KeyNotFoundException($"Staff member with ID {id} not found.");
         }
 
-        public void AddStaff(StaffMember staff)
+        public async Task AddStaffAsync(StaffMember staff)
         {
             _context.StaffMembers.Add(staff);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        public void UpdateStaff(StaffMember staff)
+        public async Task UpdateStaffAsync(StaffMember staff)
         {
             _context.StaffMembers.Update(staff);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        public void DeleteStaff(int id)
+        public async Task DeleteStaffAsync(int id)
         {
-            var staff = _context.StaffMembers.Find(id);
+            var staff = await _context.StaffMembers.FindAsync(id);
             if (staff != null)
             {
                 _context.StaffMembers.Remove(staff);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
         }
     }
