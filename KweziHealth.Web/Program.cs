f450fad6434f006c894ca9a193c63e8ea83e89c5
Tristan Wilkinson <tristanwilkinson0803@gmail.com>
2026-08-16 12:50:07 +0200

    chore: Updated the default controller route to point to StaffController.cs

--- KweziHealth.Web/Program.cs
+++ KweziHealth.Web/Program.cs
@@ -34,7 +34,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Staff/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -48,7 +48,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Staff}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.MapRazorPages()
